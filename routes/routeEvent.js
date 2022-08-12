031b7d26de3f87d0e4618dacbe43d93ae79fa995
Kolya <58349866+iluvkoohi@users.noreply.github.com>
2022-08-12 17:24:05 +0800

    Update routeEvent.js

--- routes/routeEvent.js
+++ routes/routeEvent.js
@@ -7,6 +7,7 @@ const event = require("../controllers/event/eventController");
 router.post("/event", (req, res) => event.createEvent(req, res));
 router.get("/event/all", (req, res) => event.getAllEvents(req, res));
 router.get("/event/get-by-planner", (req, res) => event.getEvents(req, res));
+router.get("/event/category/get-by-planner", (req, res) => event.getEventsByPlanner(req, res));
 router.put("/event", (req, res) => event.updateEvent(req, res));
 router.delete("/event/:id", (req, res) => event.deleteEvent(req, res));
 
