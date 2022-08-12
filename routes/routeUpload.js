b7b4b0acf8a243ef4b1cae6f2ab7ac0ef4409fb2
Kolya <58349866+iluvkoohi@users.noreply.github.com>
2022-08-12 17:40:32 +0800

    Update routeUpload.js

--- routes/routeUpload.js
+++ routes/routeUpload.js
@@ -1,1 +1,20 @@
 
+const express = require("express");
+const router = express.Router();
+
+router.post("/img", (req, res) {
+  try {
+    const accountId = req.body.accountId;
+    const filePath = req.file.path;
+    const options = {
+      folder: process.env.CLOUDINARY_FOLDER + "/planner/event",
+      unique_filename: true,
+    };
+    const uploadedImg = await cloudinary.uploader.upload(filePath, options);
+    return res.status(200).json(uploadedImg)
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
